a9e0af23b9427a5f14ea287d4efcdd085767d3fb
rahulalam31 <gaberialcramern31@outlook.com>
2024-01-18 09:49:57 +0530

    post models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasUuids,HasFactory;
+
+    protected $fillable = ['title','slug','content','tag_id'];
+    /**
+     * Get the route key for the model.
+     */
+    // public function getRouteKeyName(): string
+    // {
+    //     return 'slug';
+    // }
+    public function tag()
+    {
+        return $this->belongsTo(tag::class, 'tag_id', 'id');
+    }
 }
